222da451ce6d41daabb8008a0e123d913c6e3f8e
秋枫雁飞 <1714255949@qq.com>
2022-08-20 10:42:53 +0800

    开发模式缓存持久化也可以关闭

--- Cache/CacheFactory.php
+++ Cache/CacheFactory.php
@@ -42,7 +42,7 @@ class CacheFactory implements CacheFactoryInterface
         $this->identity = $identity;
         $this->tip      = $tip;
         $this->keep     = $permanently;
-        $this->status   = $this->config['status'][$identity]??$permanently;
+        $this->status   = DEV?($this->config['status'][$identity]??$permanently):($permanently?:$this->config['status'][$identity]??1);
     }
 
     public function isKeep(): bool
