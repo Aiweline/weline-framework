3ad5b1e47ec328008fb7d13467af200f50e44971
aiweline <aiweline@qq.com>
2023-09-28 14:59:12 +0800

    Request body参数获取优化

--- Http/Request.php
+++ Http/Request.php
@@ -132,7 +132,12 @@ class Request extends Request\RequestAbstract implements RequestInterface
             foreach (explode('&', $params) as $key => $value) {
                 $value = explode('=', $value);
                 if (count($value) === 2) {
-                    $params_[$value[0]] = $value[1] ?? '';
+                    if(str_ends_with($value[0], '%5B%5D')){
+                        $paramName = rtrim($value[0],'%5B%5D');
+                        $params_[$paramName][] = $value[1] ?? '';
+                    }else{
+                        $params_[$value[0]] = $value[1] ?? '';
+                    }
                 }
             }
             $params = $params_;
