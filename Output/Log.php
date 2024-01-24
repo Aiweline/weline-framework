6991c431f5c6a2314b133c235a19ea55946aa718
秋枫雁飞 <1714255949@qq.com>
2024-01-24 08:22:11 +0000

    add Output/Log.php.
    Signed-off-by: 秋枫雁飞 <1714255949@qq.com>

---
+++ Output/Log.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * 本文件由 秋枫雁飞 编写，所有解释权归Aiweline所有。
+ * 作者：Administrator
+ * 邮箱：aiweline@qq.com
+ * 网址：aiweline.com
+ * 论坛：https://bbs.aiweline.com
+ * 日期：24/1/2024 15:58:32
+ */
+
+namespace Weline\Framework\Output;
+
+use Weline\Framework\App\Env;
+use Weline\Framework\Output\Debug\AbstractPrint;
+
+class Log extends Debug\AbstractPrint
+{
+    public bool $out = false;
+
+    /**
+     * @DESC         |日志记录
+     *
+     * 参数区：
+     *
+     * @param             $message
+     * @param string|null $log_path
+     * @param int         $message_type
+     *
+     * @throws \Weline\Framework\App\Exception
+     */
+    public function debug($message, string $log_path = null, int $message_type = 3)
+    {
+        $this->doPrint($data, $message, self::ERROR, 0, $message_type,'debug');
+    }
+
+    /**
+     * @DESC         |信息日志记录
+     *
+     * 参数区：
+     *
+     * @param             $message
+     * @param string|null $log_path
+     * @param int         $message_type
+     *
+     * @throws \Weline\Framework\App\Exception
+     */
+    public function info($message, string $log_path = null, int $message_type = 3)
+    {
+        $this->doPrint($data, $message, self::NOTE, 0, $message_type,'info');
+    }
+}
